c18de570eeb80ecb9ff62eb0c5e54a0121c7213a
Ryan Moses <ryancmoses@outlook.com>
2024-01-08 17:08:17 -0500

    Initial Info Page

--- multi-step-form-ui/src/shared/containers/InfoPage/InfoPage.js
+++ multi-step-form-ui/src/shared/containers/InfoPage/InfoPage.js
@@ -3,7 +3,27 @@ import React from 'react';
 import './InfoPage.css';
 
 function InfoPage() {
-    
+    return (
+        <div>
+            <h2>Personal info</h2>
+            <p>Please provide your name, email address, and phone number.</p>
+
+            <form>
+                <div>
+                    <lable>Name</lable>
+                    <input placeholder='e.g. Stephen King' />
+                </div>
+                <div>
+                    <lable>Email Address</lable>
+                    <input placeholder='e.g. stephenking@lorem.com' />
+                </div>
+                <div>
+                    <lable>Phone Number</lable>
+                    <input placeholder='e.g. +1 234 567 890' />
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default InfoPage;
--- multi-step-form-ui/src/shared/containers/RouteContainer/RouteContainer.js
+++ multi-step-form-ui/src/shared/containers/RouteContainer/RouteContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import InfoPage from '../InfoPage/InfoPage';
 
 import './RouteContainer.css';
 
 function RouteContainer() {
     return (
         <div className='routes-container'>
-            Routes Container
+            <InfoPage />
         </div>
     )
 }
